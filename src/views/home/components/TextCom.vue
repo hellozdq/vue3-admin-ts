fc43885fb5c694e744ac498aff3139d674cbd57c
zdq <2608075526@qq.comm>
2021-06-20 23:58:30 +0800

    测试

---
+++ src/views/home/components/TextCom.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <el-input v-model="name.name"></el-input>
+        <!-- <el-checkbox-group v-model="name.name">
+            <el-checkbox :label="1"></el-checkbox>
+            <el-checkbox :label="2"></el-checkbox>
+        </el-checkbox-group> -->
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props:['name'],
+    setup(props) {
+        console.log(props.name)
+        props.name.name = "999"
+    },
+})
+</script>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,21 +1,24 @@
 <template>
     <div>
         {{name2}}
+        <text-com v-model:name="name2.name"></text-com>
     </div>
 </template>
 
 <script lang="ts">
 import { useStore } from 'vuex'
-import { computed, defineComponent, ref } from 'vue'
+import { computed, defineComponent, ref,reactive } from 'vue'
+import TextCom from './components/TextCom.vue'
 
 export default defineComponent({
     name: "Home",
+    components:{TextCom},
     setup(props) {
         const store = useStore();
         const name = computed(() => store.state.home.name);
-        const name2 = ref("主页");
+        const name2 = reactive({name:{name:'a222'}});
         function setStoreName(name){
-            name2.value = name;
+            // name2.value = name;
             store.dispatch('home/setName',name)
         }
         return { name, name2, setStoreName };
