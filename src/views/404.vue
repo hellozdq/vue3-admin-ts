de537e5bfdbb78e1094ea9b5d173cbfa55561f97
zdq <2608075526@qq.comm>
2021-06-28 21:47:40 +0800

    新增404页面

---
+++ src/views/404.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        404
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    font-size: 36px;
+}
+</style>
